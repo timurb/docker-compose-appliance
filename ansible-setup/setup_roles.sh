2999ea575b73d2c2456d89913f009f01c614b1d3
Timur Batyrshin <erthad@gmail.com>
2021-11-22 19:18:01 +0300

    protect appliances with HTTPAuth

--- ansible-setup/setup_roles.sh
+++ ansible-setup/setup_roles.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+# appliance_traefik.yml should be the first to be deployed to create networks
 $(dirname $0)/run_ansible.sh \
-  ./playbooks/appliance_metrics.yml \
   ./playbooks/appliance_traefik.yml \
+  ./playbooks/appliance_metrics.yml \
   ./playbooks/appliance_prometheus.yml \
   ./playbooks/appliance_grafana.yml \
   $@
