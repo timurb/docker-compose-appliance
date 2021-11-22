8067e7153cb641e1466cd78e108f113cbdbda397
Timur Batyrshin <erthad@gmail.com>
2021-11-22 12:14:06 +0300

    update inventory and setup scripts

--- ansible-setup/setup_roles.sh
+++ ansible-setup/setup_roles.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$(dirname $0)/run_ansible.sh  ./playbooks/metrics.yml ./playbooks/traefik.yml $@
+$(dirname $0)/run_ansible.sh  ./playbooks/metrics.yml ./playbooks/traefik.yml ./playbooks/prometheus.yml ./playbooks/grafana.yml $@
