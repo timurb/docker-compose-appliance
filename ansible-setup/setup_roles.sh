5542c890b99d39a5286e5a4fd52ede4a45933fde
Timur Batyrshin <erthad@gmail.com>
2021-11-22 12:19:52 +0300

    fix invalid chars in role names; rename all playbooks for appliances for easier differentiation

--- ansible-setup/setup_roles.sh
+++ ansible-setup/setup_roles.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-$(dirname $0)/run_ansible.sh  ./playbooks/metrics.yml ./playbooks/traefik.yml ./playbooks/prometheus.yml ./playbooks/grafana.yml $@
+$(dirname $0)/run_ansible.sh \
+  ./playbooks/appliance_metrics.yml \
+  ./playbooks/appliance_traefik.yml \
+  ./playbooks/appliance_prometheus.yml \
+  ./playbooks/appliance_grafana.yml \
+  $@
