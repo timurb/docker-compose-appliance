c6ab1d791cc33c7505ca9109988df82dde4f54ac
Timur Batyrshin <erthad@gmail.com>
2021-11-27 18:00:19 +0300

    added basic logging support

--- ansible-setup/setup_roles.sh
+++ ansible-setup/setup_roles.sh
@@ -2,8 +2,9 @@
 
 # appliance_traefik.yml should be the first to be deployed to create networks
 $(dirname $0)/run_ansible.sh \
-  ./playbooks/appliance_docker_system.yml \
   ./playbooks/appliance_traefik.yml \
+  ./playbooks/appliance_docker_system.yml \
+  ./playbooks/appliance_seq.yml \
   ./playbooks/appliance_metrics.yml \
   ./playbooks/appliance_prometheus.yml \
   ./playbooks/appliance_grafana.yml \
