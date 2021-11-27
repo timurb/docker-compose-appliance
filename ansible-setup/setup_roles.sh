3942f894b7ec373f7d3444723ad3068814c58330
Timur Batyrshin <erthad@gmail.com>
2021-11-27 16:52:40 +0300

    added docker-hosted for resolving of containers from host

--- ansible-setup/setup_roles.sh
+++ ansible-setup/setup_roles.sh
@@ -2,6 +2,7 @@
 
 # appliance_traefik.yml should be the first to be deployed to create networks
 $(dirname $0)/run_ansible.sh \
+  ./playbooks/appliance_docker_system.yml \
   ./playbooks/appliance_traefik.yml \
   ./playbooks/appliance_metrics.yml \
   ./playbooks/appliance_prometheus.yml \
