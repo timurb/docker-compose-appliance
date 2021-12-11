b4b7194ccbee2b02bab4b6bb00d318274f8f7d7f
Timur Batyrshin <erthad@gmail.com>
2021-12-11 18:09:07 +0300

    create dedicated appliance for network management

--- ansible-setup/setup_roles.sh
+++ ansible-setup/setup_roles.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# appliance_traefik.yml should be the first to be deployed to create networks
+# appliance_traefik.yml should be the first to be deployed to create networks ###FIXME doesn't work for caddy etc
 $(dirname $0)/run_ansible.sh \
+  ./playbooks/appliance_network.yml \
   ./playbooks/appliance_traefik.yml \
   ./playbooks/appliance_docker_system.yml \
   ./playbooks/appliance_seq.yml \
