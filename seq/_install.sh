dd5e8a70e22a43534958f99be251e8f00dba314a
Timur Batyrshin <erthad@gmail.com>
2021-12-19 23:42:00 +0300

    add installer for seq

---
+++ seq/_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+usage() {
+  echo "Usage: $(basename $0) INSTALL_PATH" >&2
+  exit 1
+}
+
+[ -z "$1" ] && usage
+
+docker run --rm -it --mount type=bind,source=$1,target=/install erthad/appliance-seq
