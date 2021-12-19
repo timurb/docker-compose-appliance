d3a278b0c49c9799a421f23a7f34050766556106
Timur Batyrshin <erthad@gmail.com>
2021-12-19 23:39:11 +0300

    added example Dockerfile for appliance installer

--- init-appliance.sh
+++ init-appliance.sh
@@ -10,6 +10,9 @@ usage() {
 mkdir -p $1
 
 cp -i appliance-installer/_* $1
+cp -i appliance-installer/Dockerfile.init $1/Dockerfile
 ln -is ../appliance-installer/.dockerignore $1
+
+echo
 echo "!!! Edit and update the files below !!!"
 grep -r CHANGEME $1
